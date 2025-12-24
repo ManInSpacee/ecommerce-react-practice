874283a3f9fa917593a2635e858774f76897ec4d
ManInSpace <klimentmart@gmail.com>
2025-12-24 16:48:59 +0300

    Convert home and checkout to async await

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -10,10 +10,12 @@ const HomePage = ({ cart }) => {
 
 
   useEffect(() => {
-    axios.get('/api/products')
-      .then((response) => {
-        setProducts(response.data);
-      })
+
+    const getHomeData = async () => {
+      const response = await axios.get('/api/products');
+      setProducts(response.data);
+    };
+    getHomeData();
   }, []);
 
 
